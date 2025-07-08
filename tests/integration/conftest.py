90d8e5035045ad2182c8ee8048f423ee79a20251
Tobias Günther <xyaren@users.noreply.github.com>
2025-07-08 23:09:39 +0000

    Fix tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,4 +1,5 @@
 """Fixtures for testing."""
+
 from unittest.mock import patch
 
 import pytest
@@ -9,18 +10,14 @@ from async_upnp_client.ssdp_listener import SsdpListener
 @pytest.fixture(autouse=True)
 def mock_api_client():
     """Disable Api Notify Server startup."""
-    with patch(
-        "custom_components.magentatv.media_player.Client", autospec=True, spec_set=True
-    ) as mock:
+    with patch("custom_components.magentatv.media_player.Client", autospec=True, spec_set=True) as mock:
         yield mock.return_value
 
 
 @pytest.fixture(autouse=True)
 def mock_notify_server():
     """Disable Api Notify Server startup."""
-    with patch(
-        "custom_components.magentatv.NotifyServer", autospec=True, spec_set=True
-    ) as mock:
+    with patch("custom_components.magentatv.NotifyServer", autospec=True, spec_set=True) as mock:
         yield mock.return_value
 
 
@@ -32,21 +29,25 @@ def auto_enable_custom_integrations(enable_custom_integrations):
     yield
 
 
-@pytest.fixture(scope="module", autouse=True)
-def silent_ssdp_listener():
+@pytest.fixture(autouse=True)
+async def silent_ssdp_listener():
     """Patch SsdpListener class, preventing any actual SSDP traffic."""
-    with patch("homeassistant.components.ssdp.SsdpListener.async_start"), patch(
-        "homeassistant.components.ssdp.SsdpListener.async_stop"
-    ), patch("homeassistant.components.ssdp.SsdpListener.async_search"):
+    with (
+        patch("homeassistant.components.ssdp.scanner.SsdpListener.async_start"),
+        patch("homeassistant.components.ssdp.scanner.SsdpListener.async_stop"),
+        patch("homeassistant.components.ssdp.scanner.SsdpListener.async_search"),
+    ):
         # Fixtures are initialized before patches. When the component is started here,
         # certain functions/methods might not be patched in time.
         yield SsdpListener
 
 
-@pytest.fixture(scope="module", autouse=True)
-def disabled_upnp_server():
+@pytest.fixture(autouse=True)
+async def disabled_upnp_server():
     """Disable UPnpServer."""
-    with patch("homeassistant.components.ssdp.UpnpServer.async_start"), patch(
-        "homeassistant.components.ssdp.UpnpServer.async_stop"
-    ), patch("homeassistant.components.ssdp._async_find_next_available_port"):
+    with (
+        patch("homeassistant.components.ssdp.server.UpnpServer.async_start"),
+        patch("homeassistant.components.ssdp.server.UpnpServer.async_stop"),
+        patch("homeassistant.components.ssdp.server._async_find_next_available_port"),
+    ):
         yield UpnpServer
